e45271cdb1322224f0cec8ebf529a5a1ea1a873e
kamanivenkat <venkat16042020@gmail.com>
2023-12-08 12:04:03 +0530

    hour2 code added

---
+++ src/test/java/org/xproject/accounts/Hour2.java
@@ -0,0 +1,77 @@
+package org.xproject.accounts;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.naming.Name;
+import java.time.Duration;
+
+public class Hour2 {
+    @Test
+    public void testHour2() throws InterruptedException {
+
+        String appurl= "http://demo.guru99.com/test/newtours/";
+        // Selenium: Set property for to know the location of chromedriver exe
+        System.setProperty("webdriver.chrome.driver", "browserDrivers/chromedriver-win64/chromedriver.exe");
+        // Webdriver is the interface which used to create the browser session for the
+        // particular type of browser.
+
+        //What is selenium: Selenium is opensource java package which is used to
+        // interact with web applications and to test web applications.
+
+        // What are other compitative tools to Selenium: UFT(QTP), uiPath, Tosca, etc...
+
+        //Why only selenium: It is opensource, support to write tests using multiple programming language
+        //like java, python, c#, ruby, perl etc. And it is support to test web apps on multiple browsers
+        //like chrome, firefox, safary etc..
+
+        //Write a program to login an application.
+        WebDriver driver = new ChromeDriver();
+        // Selenium: launch web application by accessing the url
+        driver.get(appurl);
+//      Thread.sleep(3000);
+//       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+        driver.findElement(By.name("userName")).sendKeys( "test");
+        driver.findElement(By.name("password")).sendKeys( "test");
+        driver.findElement(By.name("submit")).click();
+
+        boolean isLoginSuccess = driver.findElement(By.xpath("//h3[text()='Login Successfully']")).isDisplayed();
+        Assert.assertEquals(isLoginSuccess, true);
+
+        driver.findElement(By.linkText("Flights")).click();
+
+        boolean isPassingerFieldDisplayed = driver.findElement(By.xpath("//b[text()='Passengers:']")).isDisplayed();
+        Assert.assertEquals(isPassingerFieldDisplayed, true);
+        driver.findElement(By.xpath("//input[@name='tripType' and @value='oneway']")).click();
+        WebElement passingerCount = driver.findElement(By.name("passCount"));
+        Select selectObj = new Select(passingerCount);
+        selectObj.selectByIndex(2);
+
+
+
+
+
+
+
+        //What is the locator:
+        //Selenium will identify the elements using locators.
+        //Selenium offers 8 type of locators
+        //If we have straight attributes id, name; we will use them.
+        //If links having text we will use linkText or partialLink text.
+        //Majority cases, we will use xpath only.
+        /*driver.findElement(By.name("aaaaa")).click();
+        driver.findElement(By.id("aaaaa")).click();
+        driver.findElement(By.linkText("aaaa")).click();
+        driver.findElement(By.partialLinkText("aaaa")).click();
+        driver.findElement(By.tagName("aaaa")).click();
+        driver.findElement(By.className("aaa")).click();
+        driver.findElement(By.cssSelector(">a")).click();
+        driver.findElement(By.xpath("//tagName[@attributeName='attributeValue']")).click();*/
+        driver.close();
+
+    }
+}
