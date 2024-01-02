7668c62a0dc56ecee2a46e31bebb8c56d503ab7b
kamanivenkat <venkat16042020@gmail.com>
2024-01-02 10:45:37 +0530

    collections

--- src/test/java/org/xproject/learnSeleniumAndJava/javaCollections/Class1_List.java
+++ src/test/java/org/xproject/learnSeleniumAndJava/javaCollections/Class1_List.java
@@ -14,14 +14,23 @@ public class Class1_List {
         // Sort the array 'ids' elements in ascending order
         int temporary;
         for (int i = 0; i <= ids.length-1; i++) {
+            System.out.println("iteration:" + i);
             for (int j = i + 1; j <= ids.length-1; j++) {
-                if (ids[i] > ids[j]) {
-                    temporary = ids[i];
-                    ids[i] = ids[j];
-                    ids[j] = temporary;
+                if (ids[i] > ids[j]) {//2>4
+                    temporary = ids[i];//temporary:22
+                    ids[i] = ids[j];//0 th place is 4 now.
+                    ids[j] = temporary;//4th place is 22 now;4th place is
                 }
+                System.out.println("iteration of j:" + j);
+                System.out.println(Arrays.toString(ids));
             }
         }
+
+        //collection is an interface with inbuilt methods:
+        //collection interface members:
+        // contains;containsAll; add; allAll; remove; removeAll; clear; size
+
+        //ArrayList, LinkedList; HastSet; LinkedHashSet
 
         System.out.println("ascending order.......");
         System.out.println(Arrays.toString(ids));
@@ -51,8 +60,12 @@ public class Class1_List {
 //        Map g = new LinkedHashMap();
 //        Map h = new TreeMap();
 
-        a.add(2); a. add(3); a.add(38); a.add(88); a.add(12);
+        a.add(2); a. add(3); a.add(38); a.add(88); a.add(12); a.add(2);
+        System.out.println("part1....");
+        System.out.println(a);
         a.addAll(a);
+        System.out.println("part2......");
+        System.out.println();
         System.out.println(a);
         System.out.println(a.get(1));
         System.out.println("reverse");
@@ -63,6 +76,7 @@ public class Class1_List {
         System.out.println(a);
         System.out.println("reverse order");
         Collections.sort(a, Collections.reverseOrder());
+        System.out.println(a);
         System.out.println("min value: " + Collections.min(a));
         System.out.println("max value: " + Collections.max(a));
 
@@ -78,7 +92,7 @@ public class Class1_List {
         Iterator<Integer> i = a.iterator();
         Integer val;
         while (i.hasNext()){
-            val = i.next();
+            val = i.next();//read current element; move the cursor to next element
             System.out.println(val);
         }
         System.out.println("part3......");
